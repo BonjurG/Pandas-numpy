4ad7aeab2d5d3c4e44fc1c39d9e0f06fa920f05d
BonjurG <hmgrem@yandex.ru>
2022-08-20 21:22:27 +0500

    more solutions

--- nine.py
+++ nine.py
@@ -4,3 +4,12 @@ import pandas as pd
 
 df = pd.read_xml('users.xml')  # загружаем html-файл
 df.loc[(df['sex'] == 'F') & (df['blood_group'] == 'B+'), 'name'].count()
+
+
+
+#one more
+#df.query(" sex=='F' ").value_counts("blood_group")
+
+
+#more
+#data.query("blood_group == 'B+' and sex == 'F'").count()[0]
